085e721f31beb5c5223b792549518b3efa640e27
xaviguasch <xaviguasch@gmail.com>
2018-10-20 17:12:31 +0200

    refactoring old code to javascript modules

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# boilerplate
 # notes-app-new
+
+Refactored version of notes-app-old (adding webpack, babel, javascript modules and 3rd party libraries).
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import { getNotes } from './notes'
+import { createNote } from './notes'
+import { setFilters } from './filters'
+import { renderNotes } from './views'
 
-console.log(getNotes());
+renderNotes()
+
+document.querySelector('#create-note').addEventListener('click', (e) => {
+    const id = createNote() // the createNote function returns the ID
+    location.assign(`/edit.html#${id}`)
+})
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    setFilters({
+        searchText: e.target.value
+    })
+    renderNotes()
+})
+
+document.querySelector('#filter-by').addEventListener('change', (e) => {
+    setFilters({
+        sortBy: e.target.value
+    })
+    renderNotes()        
+})
+
+window.addEventListener('storage', (e) => {
+    if (e.key === 'notes') {
+        renderNotes()  
+    }
+})
